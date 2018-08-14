84f596e924e361251cbbd380a09100c7d26687b7
Erikadionn <erikadionn@gmail.com>
2018-08-14 11:44:34 -0400

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to who-rolled-higher
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/who_rolled_higher/core_test.clj
@@ -0,0 +1,7 @@
+(ns who-rolled-higher.core-test
+  (:require [clojure.test :refer :all]
+            [who-rolled-higher.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
